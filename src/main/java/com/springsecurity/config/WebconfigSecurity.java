fe037746f7813a6bc5b8767488da095efb3e548e
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-26 09:22:20 -0300

    Criando WelcomeController e definindo os acessos dos usuários.

--- src/main/java/com/springsecurity/config/WebconfigSecurity.java
+++ src/main/java/com/springsecurity/config/WebconfigSecurity.java
@@ -2,6 +2,7 @@ package com.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebconfigSecurity {
 
     @Bean
