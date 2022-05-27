1b4e444e7c6d2ad69c54c23f9e497bbecec68440
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-27 20:38:23 -0300

    Criando mapeamento objeto relacional de usuário

---
+++ src/main/java/com/springsecurity/model/domain/User.java
@@ -0,0 +1,44 @@
+package com.springsecurity.model.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tab_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "id_user")
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tab_user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role_id")
+    private List<String> roles = new ArrayList<>();
+
+    public User(String username) {
+        this.username = username;
+    }
+}
+
