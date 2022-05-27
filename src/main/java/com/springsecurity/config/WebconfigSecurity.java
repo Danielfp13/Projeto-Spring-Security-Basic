b89ae26b9d86cec192147cebc2698d6927b0b33d
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-27 20:50:09 -0300

    Autenticação de usuários do banco de dados

--- src/main/java/com/springsecurity/config/WebconfigSecurity.java
+++ src/main/java/com/springsecurity/config/WebconfigSecurity.java
@@ -1,24 +1,55 @@
 package com.springsecurity.config;
 
+import com.springsecurity.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.lang.reflect.Method;
-
 @Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebconfigSecurity {
 
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeHttpRequests()
+            .antMatchers(HttpMethod.GET, "/").permitAll()
+            .antMatchers("login").permitAll()
+            .antMatchers("admins").hasAnyRole("ADMIN")
+            .antMatchers("/users").hasAnyRole("USER")
+            .antMatchers("/usersandadmins").hasAnyRole("USER", "ADMIN")
+            .anyRequest().authenticated()
+            .and().httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+/*    @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.builder()
                 .username("user")
@@ -31,23 +62,5 @@ public class WebconfigSecurity {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-           .authorizeHttpRequests()
-                   .antMatchers(HttpMethod.GET, "/").permitAll()
-                   .antMatchers("login").permitAll()
-                   .antMatchers("admins").hasAnyRole("ADMIN")
-                   .antMatchers("/users").hasAnyRole("USER")
-                   .antMatchers("/usersandadmins").hasAnyRole("USER","ADMIN")
-           .anyRequest().authenticated().and().formLogin();
-        return http.build();
-    }
-
-    @Bean
-    BCryptPasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
+    }*/
 }
